1d4feb10b058e91c290a0614b5e4d17c97cf2e1e
Diana Marin <marin_diana_madalina@yahoo.com>
2025-06-30 12:59:15 +0300

    Day 1: Git Setup, Docker Configuration & Laravel+Livewire Installation

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Livewire App</title>
+    @livewireStyles
+</head>
+<body>
+    @yield('content')
+    @livewireScripts
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Salut din Laravel + Livewire!</h1>
+    <livewire:counter />
+@endsection
